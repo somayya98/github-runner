d8d9adc6e05767bc9d5036e0d9ceaa217676877d
somayya <somayya.adusumalli98@gmail.com>
2026-02-07 14:33:02 +0530

    github-actions

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "somayya.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0775499YCLL41QUIF01"
+  description = "description"
+}
